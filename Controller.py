bae97280e63e4eb9f55ff6f2280f0bfcf36e54f1
Rick Myers <1986092+Rick-Myers@users.noreply.github.com>
2019-02-08 20:40:31 -0500

    Basic controller added. Not currently functioning.

--- Controller.py
+++ Controller.py
@@ -1,1 +1,17 @@
-#Nothing to see here.
+import sys
+
+class GiftCardController(object):
+
+	def __init__(self):
+		#instantiate model
+		#instantiate view
+	
+	def handle(self, request):
+		#begin handling gift card requests
+
+def main(main):
+	request_handler = GiftCardController()
+	request_handler.handle(name)
+
+if __name__ == "__main__":
+	#begin execution with some data from user
