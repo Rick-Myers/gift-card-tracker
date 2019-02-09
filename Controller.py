f44b234d3b9fee98d60e706c430d1c813d530764
Rick Myers <1986092+Rick-Myers@users.noreply.github.com>
2019-02-09 00:43:25 -0500

    Added main menu. Currently resides in View.

--- Controller.py
+++ Controller.py
@@ -1,17 +1,23 @@
 import sys
+from View import GiftCardView
+from Model import GiftCardModel
 
 class GiftCardController(object):
 
 	def __init__(self):
-		#instantiate model
-		#instantiate view
+		self.model = GiftCardModel()
+		self.view = GiftCardView()
 	
 	def handle(self, request):
-		#begin handling gift card requests
+		if (request == 'start'):
+			self.view.generate_main_menu()
+	
+	
+	
 
-def main(name):
+def main():
 	request_handler = GiftCardController()
-	request_handler.handle(name)
+	request_handler.handle('start')
 
 if __name__ == "__main__":
-	#begin execution with some data from user
+	main()
--- View.py
+++ View.py
@@ -1,6 +1,32 @@
 class GiftCardView(object):
-	
-	def __init__(self):
-		pass
-	
-	
+
+    def __init__(self):
+        pass
+
+    def list_all_cards(self):
+        print("list all cards")
+
+    def add_a_card(self):
+        print("add a card")
+
+    def update_balance(self):
+        print("update balance")
+
+    def exit(self):
+        print("Exiting")
+
+    def generate_main_menu(self):
+        #todo move to model, retrieve through controller
+        testList = [
+            {"List Cards":self.list_all_cards},
+            {"Add Card":self.add_a_card},
+            {"Update Balance":self.update_balance},
+            {"Exit":self.exit}
+        ]
+
+        print("**Gift Card Tracker**")
+        for index, value in enumerate(testList, 1):
+            print ("[" + str(index) + "] " + str(list(value.keys())[0]))
+
+#testA = GiftCardView()
+#testA.generate_main_menu()
